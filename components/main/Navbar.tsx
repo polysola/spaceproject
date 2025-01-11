c3e668b10611fc96894e8ac105d7a66ab01f7884
NFTGameA <joehirest72@gmail.com>
2025-01-11 23:27:47 +0700

    fix

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -24,7 +24,8 @@ const Navbar = () => {
         </a>
 
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+          <div className="hidden lg:flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+            {" "}
             <a href="#overview" className="cursor-pointer">
               Overview
             </a>
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -61,12 +61,7 @@ const HeroContent = () => {
         variants={slideInFromRight(0.8)}
         className="w-full h-full flex justify-center items-center"
       >
-        <Image
-          src="/mainIconsdark.svg"
-          alt="work icons"
-          height={650}
-          width={650}
-        />
+        <Image src="/bg.png" alt="work icons" height={650} width={650} />
       </motion.div>
     </motion.div>
   );
